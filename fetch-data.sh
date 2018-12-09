0021c6a2d728e87424cd29bf49186dd040ea50a0
Andrej Staruch <staruch.andrej@gmail.com>
2018-12-09 20:45:38 +0100

    fix extension

--- fetch-data.sh
+++ fetch-data.sh
@@ -2,7 +2,7 @@
 
 URL='http://data.phishtank.com/data/cdc400e08a81d1e231cfd3ea919291c8395f7c0289421671dff35be88b3b7558/online-valid.csv.bz2'
 TODAY=`date +'%Y-%m-%d'`
-FILENAME=$TODAY.cvs.bz2
+FILENAME=$TODAY.csv.bz2
 COMMAND="wget -O $FILENAME $URL"
 DATA_DIR="./data"
 
